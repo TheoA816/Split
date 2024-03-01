cfcb4e3b10fee5c4e0443b53d6db1f49a755eed9
Michael Girikallo <michael.girikallo@gmail.com>
2024-03-02 03:56:06 +1100

    Friends frontend page

--- app/friends/friend.tsx
+++ app/friends/friend.tsx
@@ -3,20 +3,21 @@ import Image from 'next/image';
 import { TrashIcon } from '@heroicons/react/24/solid';
 
 interface FriendProps {
-  image: string;
+  profilepicture: string;
   name: string;
 }
 
-const FriendComponent: FC<FriendProps> = ({ image, name }) => {
+const FriendComponent: FC<FriendProps> = ({ profilepicture, name }) => {
   return (
-    <div className='flex justify-between bg-white h-[72px] rounded-2xl shadow-lg backdrop-blur'>
+    <div className='flex justify-between bg-white h-[72px] rounded-2xl shadow-lg backdrop-blur mt-3 min-w-60'>
 
-      <div className='flex pl-6 py-4 w-[154px] items-center'>
-        <Image src={image} alt={name} width={40} height={40} className='rounded-full'/>
-        <h1 className='pl-3 font-bold text-16 font-balsamiq-sans text-splitDarkBlue'>{name}</h1>
-      </div>
-
-      <div className='flex pr-6 items-center'>
+      <button className='flex-shrink-0'>
+        <div className='flex pl-6 py-4 items-center'>
+          <Image src={profilepicture} alt={name} width={40} height={40} className='rounded-full'/>
+          <h1 className='pl-3 font-bold text-base font-balsamiq-sans text-splitDarkBlue'>{name}</h1>
+        </div>
+      </button>
+      <div className='flex pr-6 items-center flex-shrink-0'>
         <button>
           <TrashIcon className='w-6 h-6 text-splitDarkBlue'></TrashIcon>
         </button>
