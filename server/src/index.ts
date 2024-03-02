a7d43d2c96f28bb68177166da51661f13cadaec5
Merry Rosalie <z5391208@ad.unsw.edu.au>
2024-03-02 17:34:06 +1100

    feature: list all friends + histories + logout

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,16 +4,19 @@ import errorHandler from "middleware-http-errors";
 import { validateRequest, verifySession } from "./lib/middleware";
 import { UserSignupSchema } from "./schema/user.schema";
 import { signoutUser, signupUser } from "./functions/auth";
+import prisma from "./lib/prisma";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post('/auth/signup', validateRequest(UserSignupSchema, "body"), async (req: Request, res: Response, next: NextFunction) => {
+app.post(
+  "/auth/signup",
+  validateRequest(UserSignupSchema, "body"),
+  async (req: Request, res: Response, next: NextFunction) => {
     console.log("Responding to POST /auth/login");
     try {
       const { username, password } = req.body;
@@ -26,7 +29,8 @@ app.post('/auth/signup', validateRequest(UserSignupSchema, "body"), async (req: 
     } catch (err) {
       next(err);
     }
-});
+  }
+);
 
 app.post(
   "/auth/signout",
@@ -43,8 +47,66 @@ app.post(
   }
 );
 
+app.get(
+  "/friends",
+  verifySession,
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("Responding to GET /friends");
+    try {
+      const friends = await prisma.user.findMany({
+        where: {
+          id: Number(req.headers.id),
+        },
+        select: {
+          friends: {
+            select: {
+              email: true,
+              name: true,
+              profilePicture: true,
+            },
+          },
+        },
+        orderBy: {
+          name: "asc",
+        },
+      });
+      return { friends };
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
+app.get("/history", verifySession, async (req: Request, res: Response, next: NextFunction) => {
+    console.log("Responding to GET /history");
+    try {
+      const histories = await prisma.bill.findMany({
+        where: {
+          userId: Number(req.headers.id),
+        },
+        select: {
+          title: true,
+          userId: true,
+          completed: true,
+          createdAt: true,
+          items: {
+            select: {
+              name: true,
+              cost: true
+            }
+          }
+        },
+        orderBy: {
+          title: "asc",
+        },
+      });
+      return { histories };
+    } catch (err) {
+      next(err);
+    }
+
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
